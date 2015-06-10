3b5d8d67a7df8d58c07df9182e33f341d721f955
dotslashbin <joshuarpf@gmail.com>
2015-06-10 16:28:16 +0800

    Added implementation of delete, form for adding, and basic authentication

--- Wine-Uploader.js
+++ Wine-Uploader.js
@@ -2,26 +2,34 @@ Wines = new Mongo.Collection("Wines");
 
 if (Meteor.isClient) {
   // counter starts at 0
-  Template.body.helpers(
-    {
+  Template.body.helpers({
         Wines:function() {
           return Wines.find({});
         }
-    }
-  );
+  });
 
-  Template.body.events(
-    {
+  Template.body.events({
         "submit .new-wine":function(event) {
+            var name      = event.target.wine_name.value;
+            var vintage  = event.target.wine_vintage.value;
 
-            var newWine = { name:"generated1", vintage:"1000" };
-
+            var newWine   = { name: name, vintage: vintage, created_by: Meteor.user() };
             Wines.insert(newWine);
-
             return false;
+        },
+
+        "click .test-button":function(event) {
+            alert("Someone clicked on the test");
+        },
+
+        "click .delete":function() {
+          Wines.remove(this._id); 
         }
-    }
-  )
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
 }
 
 if (Meteor.isServer) {
